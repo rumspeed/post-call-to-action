2212645c9cb85f1d330fbc1785b59404202b0f88
Scot Rumery <scot@rumspeed.com>
2014-09-18 14:07:33 -0400

    plugin file shell

---
+++ post-call-to-action.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Plugin Name: Post Call to Action
+Description: Every page should do something. Display a call to action bar below each blog post.
+Version: 0.1.0
+Author: Scot Rumery
+Author URI: http://rumspeed.com/scot-rumery/
+*/
+
+
+
+
+define( 'RUM_POST_CTA_PLUGIN_VERSION', '0.0.1' );
+define( 'RUM_POST_CTA_PLUGIN_DIR', dirname( __FILE__ ) );
+define( 'RUM_POST_CTA_PLUGIN_URI', plugins_url( '' , __FILE__ ) );
+
+
+
+
+// include files - these are simply to organize functions into logical areas
+//include_once( 'includes/settings.php' );
+//include_once( 'includes/display-functions.php' );
+
