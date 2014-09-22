85ba9cf5af3a85aebfaf2a8ce6604d1defb6659f
Julie Kuehl <julie@juliekuehl.com>
2014-09-22 15:23:42 -0500

    Added some comments to indicate intent

--- includes/post-call-to-action-box.php
+++ includes/post-call-to-action-box.php
@@ -1,7 +1,12 @@
 <!-- create the CTA box -->
 <div class="rum-cta-box">
+	<div class="">
+		<!-- display the featured image from the embedded post/page/etc. -->
+		<?php rum_post_cta_image() ?>
+	</div>
     <div class="rum-cta-text">
         <!-- display the cta text -->
+	    <!-- text should be the title from the embedded post/page/etc. -->
 		<?php rum_post_cta_text() ?>
     </div>
 	<div class="rum-cta-button">
