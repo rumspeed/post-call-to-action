6edccefc31ec02128235e51deeaf80bddf89a3d4
Julie Kuehl <julie@juliekuehl.com>
2014-09-24 13:56:05 -0500

    Code cleanup continued
    Of course I missed a few things. Sigh.

--- post-call-to-action.php
+++ post-call-to-action.php
@@ -40,7 +40,7 @@ function rum_post_cta_menu() {
 
 add_action( 'admin_menu', 'rum_post_cta_menu' );
 
-/* ----- add the page for the Post Call to Action settings ----- */
+/* ----- add the page with the Post Call to Action settings ----- */
 
 function rum_post_cta_options_page() {
 
@@ -52,13 +52,7 @@ function rum_post_cta_options_page() {
 
 }
 
-/* ----- add color picker ----- */
-
-/*
- * add a color picker to the Setting screen
- * use the iris.min.js script
- *
- */
+/* ----- add color picker that can be used on the Settings screen ----- */
 
 add_action( 'admin_enqueue_scripts', 'wp_enqueue_color_picker' );
 
@@ -94,13 +88,6 @@ function wp_enqueue_color_picker( ) {
 
 /* ----- add the meta box to post sidebars ----- */
 
-	/*
-	 * add a Post Call To Action metabox to New Post and Edit Post screens
-	 * use the add_meta_box function
-	 * add_meta_box( $id, $title, $callback, $page, $context, $priority, $callback_args )
-	 *
-	 */
-
 add_action( 'add_meta_boxes', 'post_cta_meta_box_init' );
 
 function post_cta_meta_box_init() {
@@ -115,6 +102,13 @@ function post_cta_meta_box_init() {
 		foreach ( $post_types as $post_type ) {
 			echo '<p>' . $post_type . '</p>';
 		}
+
+	/*
+	 * add a Post Call To Action meta box to New Post and Edit Post screens
+	 * use the add_meta_box function
+	 * add_meta_box( $id, $title, $callback, $page, $context, $priority, $callback_args )
+	 *
+	 */
 
 	add_meta_box(
 		'post-cta-meta-box',
@@ -131,14 +125,14 @@ function post_cta_meta_box_init() {
 }
 
 
-// This function adds a meta box with a callback function of my_metabox_callback()
+// This function adds a meta box with a callback function of my_meta_box_callback()
 //function add_my_meta_box() {
 //	$var1 = 'this';
 //	$var2 = 'that';
 //	add_meta_box(
-//		'metabox_id',
-//		'Metabox Title',
-//		'my_metabox_callback',
+//		'meta_box_id',
+//		'meta_box Title',
+//		'my_meta_box_callback',
 //		'page',
 //		'normal',
 //		'low',
@@ -147,13 +141,13 @@ function post_cta_meta_box_init() {
 //}
 
 // $post is an object containing the current post (as a $post object)
-// $metabox is an array with metabox id, title, callback, and args elements.
+// $meta_box is an array with meta box id, title, callback, and args elements.
 // The args element is an array containing your passed $callback_args variables.
 
-function post_cta_metabox_callback ( $post, $metabox ) {
+function post_cta_meta_box_callback ( $post, $meta_box ) {
 	echo 'Last Modified: ' . $post->post_modified;        // outputs last time the post was modified
-	echo $metabox['args']['foo'];                         // outputs 'this'
-	echo $metabox['args']['bar'];                         // outputs 'that'
+	echo $meta_box['args']['foo'];                         // outputs 'this'
+	echo $meta_box['args']['bar'];                         // outputs 'that'
 	echo get_post_meta( $post->ID, 'my_custom_field', true ); // outputs value of custom field
 }
 
