84a9d40f88df3d99b488c2dd812f92e0917eeba3
Scot Rumery <scot@rumspeed.com>
2014-10-02 11:18:08 -0400

    add TODOs

--- includes/display.php
+++ includes/display.php
@@ -12,8 +12,10 @@
 function rum_post_cta_box( $content ) {
 
 	// checks to make sure code executes only on singles pages and inside of the main Loop
+// TODO - make sure we are using the post_type = 'post' in the ID statement below
 	if( is_singular() && is_main_query() ) {
 
+// TODO - only proceed if there is data in the meta field for storing the CTA data from the metabox
 		$rum_post_cta_box = '/includes/display-functions.php' ;
 		$content .= $rum_post_cta_box;
 
