cd2e3d13d1ee79165db1749c10db63b6b431db2d
Scot Rumery <scot@rumspeed.com>
2014-10-02 11:10:38 -0400

    move display related functions

--- post-call-to-action.php
+++ post-call-to-action.php
@@ -318,23 +318,3 @@ function change( $data ) {
 
 
 
-/*
- * DISPLAY POST CALL TO ACTION BOX SINGLE POSTS
- *
- */
-
-/* ----- insert CTA box below content ----- */
-
-function rum_post_cta_box( $content ) {
-
-	// checks to make sure code executes only on singles pages and inside of the main Loop
-	if( is_singular() && is_main_query() ) {
-
-		$rum_post_cta_box = '/includes/display-functions.php' ;
-		$content .= $rum_post_cta_box;
-
-	}
-	return $content;
-}
-
-add_filter( ' the_content', 'rum_post_cta_box' );
