67dd3d04b8444acc942c46e607c605d0c0b30ed1
Scot Rumery <scot@rumspeed.com>
2014-10-02 16:11:08 -0400

    add logic to add/remove metabox based on settings

--- includes/metabox.php
+++ includes/metabox.php
@@ -10,7 +10,16 @@
 /* ----- add a Post Call-to-Action meta box to New Post and Edit Post sidebars ----- */
 function rum_post_cta_meta_box_init() {
 
-    if(get_option('rum_post_cta_active')) {
+    // get plugin option array and store in a variable
+    $plugin_option_array            = get_option( 'rum_post_cta_plugin_options' );
+
+
+    // fetch individual values from the plugin option variable array
+    $activate_flag      = $plugin_option_array[ 'activate' ];
+    $post_type          = $plugin_option_array[ 'post_type' ];
+
+
+    if( $activate_flag != '' && $post_type != '' ) {
 
         // add_meta_box( $id, $title, $callback, $page, $context, $priority, $callback_args )
         add_meta_box(
