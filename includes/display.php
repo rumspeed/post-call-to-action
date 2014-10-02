13661fe36ac1ff750d7a0ee320f3be545b7c0807
Scot Rumery <scot@rumspeed.com>
2014-10-02 11:30:26 -0400

    add function to handle html output

--- includes/display.php
+++ includes/display.php
@@ -24,3 +24,37 @@ function rum_post_cta_box( $content ) {
 }
 
 add_filter( ' the_content', 'rum_post_cta_box' );
+
+
+
+
+function rum_cta_box_html() {
+
+	// initialize variables
+	$cta_html      = '';
+
+// TODO - get the post metabox metafield that stores the post we are linking to
+
+	// display the featured image from the embedded post/page/etc.
+	$cta_html .= '	<div class="rum-post-cta-image">';
+	$cta_html .= '     Image goes here';
+	$cta_html .= '	</div>';
+
+
+	// display the cta text
+	$cta_html .= '  <div class="rum-post-cta-text">';
+	$cta_html .= '		<h2>Post Title Goes Here</h2>';
+	$cta_html .= '    </div>';
+
+	// display the cta button
+	$cta_html .= '	<div class="rum-post-cta-button">';
+	$cta_html .= '		CTA Button';
+	$cta_html .= '	</div>';
+
+
+	// CTA box div wrapper
+	$cta_html .= '<div class="rum-post-cta-box">' . $cta_html . '</div>';
+
+	return $cta_html;
+}
+
