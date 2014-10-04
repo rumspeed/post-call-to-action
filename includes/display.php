7d3295a7ea8fb54605468a81d1c89f959daf1a9c
Scot Rumery <scot@rumspeed.com>
2014-10-04 09:18:06 -0400

    update comments

--- includes/display.php
+++ includes/display.php
@@ -12,8 +12,7 @@
 /* ----- insert CTA box below content ----- */
 function rum_post_cta_box( $content ) {
 
-	// checks to make sure code executes only on singles pages and inside of the main Loop
-// TODO - make sure we are using the post_type = 'post' in the ID statement below
+	// checks to make sure code executes only on single 'post' pages and inside of the main Loop
 	if( is_singular('post') && is_main_query() ) {
 
 // TODO - only proceed if there is data in the meta field for storing the CTA data from the metabox
